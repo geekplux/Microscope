8445d08273585d8fd1939f1cb26b13f39e75a9eb
geekplux <geekplux@qq.com>
2015-04-15 11:25:16 +0800

    Enforce post URL uniqueness

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -11,6 +11,11 @@ Template.postSubmit.events({
       // 显示错误信息并退出
       if (error)
         return alert(error.reason);
+
+      // 显示结果，跳转页面
+      if (result.postExists)
+        alert('This link has already been posted（该链接已经存在）');
+
       Router.go('postPage', {_id: result._id});
     });
   }

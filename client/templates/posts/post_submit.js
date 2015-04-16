a85d98ad0c3aeb6a2e97a7d2d3c027860630796b
geekplux <geekplux@qq.com>
2015-04-16 22:13:49 +0800

    Validate post contents on submission

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,16 @@
+Template.postSubmit.created = function() {
+  Session.set('postSubmitErrors', {});
+}
+
+Template.postSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.postSubmit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -7,12 +20,14 @@ Template.postSubmit.events({
       title: $(e.target).find('[name=title]').val()
     };
 
+    var errors = validatePost(post);
+    if (errors.title || errors.url)
+      return Session.set('postSubmitErrors', errors);
+
     Meteor.call('postInsert', post, function(error, result) {
-      // 显示错误信息并退出
       if (error)
         return throwError(error, error.reason);
 
-      // 显示结果，跳转页面
       if (result.postExists)
         throwError('This link has already been posted（该链接已经存在）');
 

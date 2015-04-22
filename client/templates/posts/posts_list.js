1d9d0d76a1f4ec992153ce1757d7bdb9d0407cc3
geekplux <geekplux@qq.com>
2015-04-22 18:05:23 +0800

    Fade items in when they are drawn

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,5 +1,11 @@
 Template.postsList.onRendered(function () {
   this.find('.wrapper')._uihooks = {
+    insertElement: function (node, next) {
+      $(node)
+        .hide()
+        .insertBefore(next)
+        .fadeIn();
+    },
     moveElement: function (node, next) {
       var $node = $(node), $next = $(next);
       var oldTop = $node.offset().top;
@@ -25,6 +31,11 @@ Template.postsList.onRendered(function () {
 
       $node.addClass('animate').css('top', 0);
       $inBetween.addClass('animate').css('top', 0);
+    },
+    removeElement: function(node) {
+      $(node).fadeOut(function() {
+        $(this).remove();
+      });
     }
   }
 });
